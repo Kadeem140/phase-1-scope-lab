73d4433eba3aea764340fb9ed5cc8725fe1664a7
Kadeem140 <Beckford.kadeem@yahoo.com>
2022-02-19 08:35:30 -0500

    MVP

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = "deem"
+
+function upperCaseCustomerName(){
+    const UpperCustomer = customerName.toUpperCase();
+    console.log(UpperCustomer, "Its working") //Works in Console.log()
+    return UpperCustomer //Does not work in return
+    
+}
+
+function setBestCustomer(){
+    let bestCustomer = "not bob"
+    console.log(bestCustomer, "setBest")
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    let bestCustomer = "maybe bob"
+    console.log(bestCustomer, "Overwrite")
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = "kadeem"
+    return leastFavoriteCustomer
+}
